4039b5d9e897f9d83bef65dfc68e9ca37e2acb86
Raoul Shah <74040700+Raoul-Shah@users.noreply.github.com>
2021-05-12 12:18:22 +0200

    Added Media Query

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-fu
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
+
+function toggleMenu() {
+    const menuToggle = document.querySelector('.toggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+}
